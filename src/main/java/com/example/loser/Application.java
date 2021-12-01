5cdb43db9645052118a3157e7b75607d425cbe66
LLB11279 <llb11279@cnki.net>
2021-12-01 21:48:51 +0800

    初始化

--- src/main/java/com/example/loser/Application.java
+++ src/main/java/com/example/loser/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoserApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoserApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
