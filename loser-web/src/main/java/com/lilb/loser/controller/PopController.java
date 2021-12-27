d65a0ea42496485bcf015a607de380f64e88429f
LLB11279 <llb11279@cnki.net>
2021-12-27 16:55:03 +0800

    初始化

--- loser-web/src/main/java/com/lilb/loser/controller/PopController.java
+++ loser-web/src/main/java/com/lilb/loser/controller/PopController.java
@@ -1,5 +1,9 @@
 package com.lilb.loser.controller;
 
+import com.lilb.loser.entity.Student;
+import com.lilb.loser.mapper.StudentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class PopController {
+
+    @Autowired
+    private StudentMapper studentMapper;
+    @RequestMapping("insert")
+    public void insert(){
+        Student student = new Student();
+        student.setName("dl");
+        student.setAge(25);
+        studentMapper.insert(student);
+    }
 }
