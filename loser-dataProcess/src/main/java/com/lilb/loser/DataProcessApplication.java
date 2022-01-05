1256fc37a786c451c1773aab34b3206d6bbd6749
LLB11279 <llb11279@cnki.net>
2022-01-05 14:58:10 +0800

    初始化

---
+++ loser-dataProcess/src/main/java/com/lilb/loser/DataProcessApplication.java
@@ -0,0 +1,17 @@
+package com.lilb.loser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序
+ *
+ * @author li-lb
+ * @date 2022/01/04
+ */
+@SpringBootApplication
+public class DataProcessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataProcessApplication.class, args);
+    }
+}
---
+++ loser-dataProcess/src/test/java/com/lilb/loser/DataProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilb.loser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
