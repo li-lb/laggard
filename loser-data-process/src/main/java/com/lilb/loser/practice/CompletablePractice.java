7f791a43c88a017c0e1b3c56bc3a9af6d2454bc0
LLB11279 <llb11279@cnki.net>
2022-03-08 11:14:26 +0800

    并发问题

---
+++ loser-data-process/src/main/java/com/lilb/loser/practice/CompletablePractice.java
@@ -0,0 +1,34 @@
+package com.lilb.loser.practice;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompletablePractice {
+
+    public static void main(String[] args) throws InterruptedException {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
+            System.out.println("第一个");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "1";
+        });
+
+        completableFuture.whenCompleteAsync((s,e)->{
+
+            try {
+                System.out.println("completableFuture"+Thread.currentThread());
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println(s);
+            System.out.println(e.getMessage());
+        });
+        System.out.println("main");
+        Thread.sleep(2000);
+
+
+    }
+}
--- loser-provider/src/test/java/com/lilb/loser/RedissonApplicationTests.java
+++ loser-provider/src/test/java/com/lilb/loser/RedissonApplicationTests.java
@@ -9,7 +9,7 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 @SpringBootTest
 @Slf4j
@@ -47,6 +47,30 @@ class RedissonApplicationTests {
             System.out.println("return value:"+v+"  exception:"+e);
         });
 
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        final CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            System.out.println("this is lambda supplyAsync");
+            System.out.println("supplyAsync 是否为守护线程 " + Thread.currentThread().isDaemon());
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("this lambda is executed by forkJoinPool");
+            return "result1";
+        });
+        final CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("this is task with executor");
+            System.out.println("supplyAsync 使用executorService 时是否为守护线程 : " + Thread.currentThread().isDaemon());
+            return "result2";
+        }, executorService);
+        System.out.println(completableFuture.get());
+        System.out.println(future.get());
+        executorService.shutdown();
     }
 
 }
